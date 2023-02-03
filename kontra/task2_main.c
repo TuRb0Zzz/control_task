fe53ca945223af4e0ff725fd3173b698c5ce5e49
TuRb0Zzz <115900101+TuRb0Zzz@users.noreply.github.com>
2023-02-04 00:14:53 +0300

    Create task2_main.c

---
+++ kontra/task2_main.c
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#include "work.h"
+
+int main()
+{
+    int ar[]={1,2,4,5,6,4,5,1,7,0,1,5,7,9,1,7};
+    getMax(ar,16);
+    getMin(ar,16);
+    return 0;
+}
