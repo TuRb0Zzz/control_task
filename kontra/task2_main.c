040a0cdb212a18d3718c8309b67dfbc30171cdd1
TuRb0Zzz <115900101+TuRb0Zzz@users.noreply.github.com>
2023-02-04 17:48:48 +0300

    Update task2_main.c

--- kontra/task2_main.c
+++ kontra/task2_main.c
@@ -8,6 +8,6 @@ int main()
     int ar[]={1,2,4,5,6,4,5,1,7,0,1,5,7,9,1,7};
     cout<<getMax(ar,16)<<endl;
     int d = foo();
-    cout<<d<<endl;
+    cout<<"recursion depth "<<d<<endl;
     return 0;
 }
